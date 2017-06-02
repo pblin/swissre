76d1c283c751c44d6135255ccc139ab28415c791
Lin, Bernard <bernardlin@outlook.com>
2017-06-02 14:12:27 +0800

    update webabi.js

--- js/webabi.js
+++ js/webabi.js
@@ -1,13 +1,14 @@
 
-web3 = new Web3(new Web3.providers.HttpProvider('https://ropsten.infura.io'));
+web3 = new Web3(new Web3.providers.HttpProvider('http://africajmj.eastus.cloudapp.azure.com:8545'));
 
 var county = function(){
-  var address = '0x7698cd50ce758b28c9101336c9e8565feb4c3f02';
-  var abi = [{"constant":true,"inputs":[{"name":"","type":"bytes32"}],"name":"counties","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"drougInsurancePayout","outputs":[],"payable":false,"type":"function"}];
+
+  var address = '0x7fe6ef7adbc7fbd7b7cbe1f45714880141fe979c';
+  var abi = [{"constant":false,"inputs":[{"name":"countyName","type":"bytes32"}],"name":"getpayout","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"drougtInsurancePayout","outputs":[],"payable":false,"type":"function"}]
   var contract = web3.eth.contract(abi).at(address);
   var county = document.getElementById('counties').value;
 
-  contract.counties.call(county, function(e,r){
+  contract.getpayout.call(county, function(e,r){
     console.log(r.toNumber());
     document.getElementById('countyData').innerHTML = r.toNumber();
   })
